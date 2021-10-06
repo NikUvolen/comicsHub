c686561bbb857226507c4e403ec8bb14cc6acaff
Mikhail <mixa2003777@gmail.com>
2021-10-06 11:14:03 +0800

    ver 0.0.2 (user_panel, exit, views auth forms)

--- mysite/register_signin/views.py
+++ mysite/register_signin/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+
+from .forms import UserRegistrationForm, UserCreationForm
 
 
-def register(request):
-    return render(request, 'register_signin/register_page.html')
+def authentication(request):
+    def valid_form(form, redirect_path_name):
+        if form.is_valid():
+            form.save()
+            return redirect(redirect_path_name)
+
+    if request.method == "POST":
+        if request.POST.get('submit') == 'sign_in':
+            registration_form = UserRegistrationForm(request.POST)
+            valid_form(registration_form, 'authenticate')
+        elif request.POST.get('submit') == 'registration':
+            sign_in_form = UserCreationForm(request.POST)
+            valid_form(sign_in_form, 'home')
+    else:
+        registration_form = UserRegistrationForm()
+        sign_in_form = UserCreationForm()
+
+    context = {
+        'registration_form': registration_form,
+        'sign_in_form': sign_in_form
+    }
+
+    return render(request, 'register_signin/register_page.html', context=context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('authenticate')
